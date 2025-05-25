4e19be5a7faaa64903cab3c09f6718ec109da5d7
Reza Rasendriya <reza58912@gmail.com>
2025-05-25 17:07:49 +0700

    ngilangin import yg ga perlu

--- src/View/Dosen/ViewData.java
+++ src/View/Dosen/ViewData.java
@@ -6,8 +6,6 @@ package View.Dosen;
 
 import Controller.ControllerDosen;
 import Model.Dosen.ModelDosen;
-import View.Dosen.EditData;
-import View.Dosen.InputData;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
--- src/View/HomePage.java
+++ src/View/HomePage.java
@@ -3,9 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package View;
-
-import View.Mahasiswa.ViewData;
-
 /**
  *
  * @author ASUS
